0e323caaca29a82077ec33d05d6bdbd22bff1f59
110414 <aitor.martinez@tecnalia.com>
2023-06-06 14:25:09 +0200

    Main configurado para que cuando lo corras te guarde las predicciones de las imagenes que metas en datasets/pruebas

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from ultralytics import YOLO
 
 
@@ -12,9 +14,10 @@ def main():
     # metrics = model.val()  # evaluate model performance on the validation set
     # results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
     
-    results = model("https://www.imfdb.org/images/thumb/b/b6/AB-46.jpg/300px-AB-46.jpg") 
-    results = model("https://www.imfdb.org/images/b/bc/AO63.jpg") 
-    results = model("https://www.imfdb.org/images/thumb/7/7c/Kalashnikov2020_TT-33-1.jpg/600px-Kalashnikov2020_TT-33-1.jpg") 
+    # results = model("https://www.imfdb.org/images/thumb/b/b6/AB-46.jpg/300px-AB-46.jpg") 
+    # results = model("https://www.imfdb.org/images/b/bc/AO63.jpg") 
+    # results = model("https://www.imfdb.org/images/thumb/7/7c/Kalashnikov2020_TT-33-1.jpg/600px-Kalashnikov2020_TT-33-1.jpg")
+    results = model.predict(source=Path('datasets/pruebas'), save=True)
     
     # path = model.export(format="onnx")  # export the model to ONNX format
 
