fcc43eb6dc99109b4518b0eb5a5934a33d566975
Aitor <aitor.martinez@tecnalia.com>
2024-03-19 13:34:08 +0100

    Better training parameters and new options

--- custom_training.py
+++ custom_training.py
@@ -13,29 +13,35 @@ from ultralytics.yolo.utils import DEFAULT_CFG_PATH
 class SimpleArgumentParser(Tap):
     
     devices: List[int]  # Device to use for training on GPU. Indicate more than one to use multiple GPUs. Use -1 for CPU.
-    epochs: int  # Number of epochs to train for.
-    model: Literal["n", "s", "m", "l", "x"]  # Which variant of the model YOLO to use
-    
-    config: str = "config_for_tao_adam"
+    model: str = "yolov8"  # Model to use. Options: yolov5, yolov6, yolov8. Default: yolov8
+    model_size: Literal["n", "s", "m", "l", "x"]  # Which variant of the model YOLO to use
     dataset: str = "tao_coco"
+    # Hyperparameters
+    epochs: int  # Number of epochs to train for.
+    config: str = "config_for_tao_adam"
     lr: float = 0.001  # initial learning rate (i.e. SGD=1E-2, Adam=1E-3)
     lrf: float = 0.01  # final learning rate (lr0 * lrf)
     cos_lr: bool = False  # Use cosine learning rate
     freeze_backbone: bool = False  # Freeze the backbone of the model
     batch_size: int = 16  # Batch size.
-    val_every: int = 1  # Validate every n epochs
-    workers: int = 10  # Number of background threads used to load data.
     close_mosaic: int = 20  # Close mosaic augmentation
     from_scratch: bool = False # Train the model from scratch, not pretrained in COCO.
+    imagenet_pretrained_backbone: bool = False  # Use the pretrained backbone from ImageNet. Assumes rest of the model is from scratch.
+    # Training options
+    model_path: str = ''  # Relative path to the model you want to use as a starting point. Deactivates using model_size.
+    workers: int = 10  # Number of background threads used to load data.
+    val_every: int = 1  # Validate every n epochs
     do_not_val_during_training: bool = False  # If passed, the model is NOT validated during training.
-    model_path: str = ''  # Relative path to the model you want to use as a starting point. Deactivates using sizes.
     val_only: bool = False  # If passed, the model is only validated and not trained.
-    owod_task: Literal["", "t1", "t2", "t3"] = ""  # OWOD task to train on. 
+    # OWOD
+    owod_task: Literal["", "t1", "t2", "t3"] = ""  # OWOD task to train on.
+    
 
     def configure(self):
         self.add_argument("-e", "--epochs", required=False)
         self.add_argument("-m", "--model", required=False)
         self.add_argument("-cl_ms", "--close_mosaic")
+        self.add_argument("-im_pt_bck", "--imagenet_pretrained_backbone", action="store_true")
 
     def process_args(self):
         if not self.epochs:
@@ -45,15 +51,30 @@ class SimpleArgumentParser(Tap):
             print('Loading model from', self.model_path)
             print('Ignoring args --model --from_scratch')
             self.from_scratch = False
-            self.model = ''
+            self.model_size = ''
         else:
-            if self.model == '':
+            if self.model_size == '':
                 raise ValueError("You must pass a model size.")
         
         if 'owod' in self.dataset:
             if self.owod_task == '':
                 raise ValueError("You must pass a OWOD task")
+
+        if self.imagenet_pretrained_backbone:
+            assert self.model == "yolov8", "The pretrained backbone from ImageNet is only available for YOLOv8"
+            self.from_scratch = True
+            print("Using the pretrained backbone from ImageNet. The rest of the model is trained from scratch.")
         
+
+def select_number_of_classes(owod_task: str) -> int:
+    if owod_task == 't1':
+        return 20
+    elif owod_task == 't2':
+        return 40
+    elif owod_task == 't3':
+        return 60
+    else:
+        return 0  # 0 means that the number of classes is not going to be changed
 
 def main():
 
@@ -86,18 +107,28 @@ def main():
     else:
         if args.from_scratch:
             # build a new model from scratch
-            model = YOLO(f"yolov8{args.model}.yaml", task='detect')
+            model = YOLO(f"{args.model}{args.model_size}.yaml", task='detect')
             string_for_folder = "from_scratch"
         else:
             # Use pretrained model on COCO
-            model = YOLO(f"yolov8{args.model}.pt")  # https://docs.ultralytics.com/es/yolov5/tutorials/train_custom_data/#3-select-a-model
+            model = YOLO(f"{args.model}{args.model_size}.pt")  # https://docs.ultralytics.com/es/yolov5/tutorials/train_custom_data/#3-select-a-model
             string_for_folder = "pretrained"
+
+    # TODO: Add the option to plug in the Imagenet pretrained backbone in YOLOv8
+    if args.imagenet_pretrained_backbone:
+        imagenet_pretrained_yolov8 = YOLO(f"yolov8{args.model_size}-cls.pt")
+        imagenet_model_weights = imagenet_pretrained_yolov8.model.model[:7].state_dict()  # We can only load until layer 6
+        load_process_info = model.model.model.load_state_dict(imagenet_model_weights, strict=False)
+        print("Pretrained backbone from ImageNet plugged in YOLOv8")
 
     # Name of the folder to save the model, logs, etc.
     if project_name == 'OWOD':
-        folder_name = f'{NOW}_{args.dataset}_{args.owod_task}_yolov8{args.model}_{string_for_folder}'
+        folder_name = f'{NOW}_{args.dataset}_{args.owod_task}_{args.model}{args.model_size}_{string_for_folder}'
     else:
-        folder_name = f'{NOW}_{args.dataset}_yolov8{args.model}_{string_for_folder}'
+        folder_name = f'{NOW}_{args.dataset}_{args.model}{args.model_size}_{string_for_folder}'
+
+    # Select the number of classes for OWOD
+    number_of_classes = select_number_of_classes(args.owod_task)
 
     # TODO
     if args.freeze_backbone:
@@ -148,6 +179,7 @@ def main():
             val=not args.do_not_val_during_training,
             val_every=args.val_every,
             owod_task=args.owod_task,
+            number_of_classes=number_of_classes,
         )
 
         # Save the arguments used to a file
