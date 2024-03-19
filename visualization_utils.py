21adafdf456b142e7deb8a7cbf40bedac0c07115
Aitor <aitor.martinez@tecnalia.com>
2024-03-19 13:37:24 +0100

    new visu for ood

--- visualization_utils.py
+++ visualization_utils.py
@@ -2,9 +2,11 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 from torchvision.utils import draw_bounding_boxes
 
-def plot_results(class_names, results, folder_path: Path, now: str, valid_preds_only: bool, origin_of_idx: int, image_format='pdf', ood_decision=None, ood_method_name=''):
+def plot_results(class_names, results, folder_path: Path, now: str, valid_preds_only: bool, origin_of_idx: int,
+                 image_format='pdf', ood_decision=None, ood_method_name='', targets=None):
     # ----------------------
     ### Codigo para dibujar las cajas predichas y los targets ###
     # ----------------------
@@ -48,15 +50,52 @@ def plot_results(class_names, results, folder_path: Path, now: str, valid_preds_
         
         # If we have OoD labels, we plot the boxes with green for In-Distribution and red for OoD
         if ood_decision:
-            im = draw_bounding_boxes(
-                res.orig_img[img_idx].cpu(),
-                bboxes,
-                width=width,
-                font=font,
-                font_size=font_size,
-                labels=[f'{class_names[int(n.item())]} - {res.boxes.conf[i]:.2f}' for i, n in enumerate(labels)],
-                colors=['red' if n == 0 else 'green' for n in ood_decision[img_idx]]
-            )
+            if targets:
+                # Append bboxes of the targets and its labels
+                # Expand the ood_decision to the labels and assign the number n = -5 to the targets
+                # Then paint the targets in violet
+                
+                bboxes = torch.cat((bboxes, targets["bboxes"][img_idx]), dim=0)
+                labels = torch.cat((labels, targets["cls"][img_idx]), dim=0)
+                ood_decision_one_image = ood_decision[img_idx]
+                ood_decision_one_image = np.append(ood_decision_one_image, [-5]*len(targets["cls"][img_idx]))
+                colors = []
+                for decision in ood_decision_one_image:
+                    if decision == 1:
+                        colors.append('green')
+                    elif decision == -5:
+                        colors.append('violet')
+                    elif decision == 0:
+                        colors.append('red')
+                    else:
+                        raise ValueError("The OoD decision must be 0, 1 or -5")
+                    
+                labels_str = []
+                for i, lbl in enumerate(labels):
+                    if ood_decision_one_image[i] == -5:
+                        labels_str.append(f'{class_names[int(lbl.item())]} - GT')
+                    else:
+                        labels_str.append(f'{class_names[int(lbl.item())]} - {res.boxes.conf[i]:.2f}')
+
+                im = draw_bounding_boxes(
+                    res.orig_img[img_idx].cpu(),
+                    bboxes,
+                    width=width,
+                    font=font,
+                    font_size=font_size,
+                    labels=labels_str,
+                    colors=colors
+                )
+            else:
+                im = draw_bounding_boxes(
+                    res.orig_img[img_idx].cpu(),
+                    bboxes,
+                    width=width,
+                    font=font,
+                    font_size=font_size,
+                    labels=[f'{class_names[int(n.item())]} - {res.boxes.conf[i]:.2f}' for i, n in enumerate(labels)],
+                    colors=['red' if n == 0 else 'green' for n in ood_decision[img_idx]]
+                )
         
         # Just plot normal predictions
         else:
