1dfe1decb5f981e63c845b525a0694b1ca022f02
Aitor <aitor.martinez@tecnalia.com>
2024-03-19 13:38:53 +0100

    New scripts

--- scripts/train_owod.sh
+++ scripts/train_owod.sh
@@ -6,11 +6,19 @@ timestamp=$(date +%Y%m%d-%H%M%S)
 ## OWOD ##
 ### Scratch ###
 # Task 1
-nohup python custom_training.py -e 500 --lr 0.001 --lrf 0.1 --owod_task t1 --model s --devices 1 --dataset owod --batch_size 16 -cl_ms 5 --workers 15 --val_every 5 --from_scratch > logs/{$timestamp}_nohup_yolov8n_OWOD_t1_scratch.log 2>&1 &
-nohup python custom_training.py -e 300 --lr 0.005 --lrf 0.05 --owod_task t1 --model l --devices 2 --dataset owod --batch_size 16 -cl_ms 5 --workers 12 --val_every 5 --from_scratch > logs/{$timestamp}_nohup_yolov8l_OWOD_t1_scratch_lr0005.log 2>&1 &
-nohup python custom_training.py -e 300 --lr 0.001 --lrf 0.1 --owod_task t1 --model l --devices 3 --dataset owod --batch_size 16 -cl_ms 5 --workers 12 --val_every 5 --from_scratch > logs/{$timestamp}_nohup_yolov8l_OWOD_t1_scratch_lr0001.log 2>&1 &
-nohup python custom_training.py -e 300 --lr 0.0005 --lrf 0.1 --owod_task t1 --model l --devices 2 --dataset owod --batch_size 16 -cl_ms 5 --workers 12 --val_every 5 --from_scratch > logs/{$timestamp}_nohup_yolov8l_OWOD_t1_scratch_lr00005.log 2>&1 &
-nohup python custom_training.py -e 300 --cos_lr --lr 0.001 --lrf 0.1 --owod_task t1 --model l --devices 1 --dataset owod --batch_size 16 -cl_ms 5 --workers 12 --val_every 5 --from_scratch > logs/{$timestamp}_nohup_yolov8l_OWOD_t1_scratch_lr0001_cosineLR.log 2>&1 &
+nohup python custom_training.py -e 400 --lr 0.001 --lrf 0.1 --owod_task t1 --model yolov8 --model_size s --devices 1 --dataset owod --batch_size 16 -cl_ms 5 --workers 15 --val_every 5 --from_scratch > logs/{$timestamp}_nohup_yolov8n_OWOD_t1_scratch.log 2>&1 &
+nohup python custom_training.py -e 300 --lr 0.005 --lrf 0.05 --owod_task t1 --model yolov8 --model_size l --devices 2 --dataset owod --batch_size 16 -cl_ms 5 --workers 12 --val_every 5 --from_scratch > logs/{$timestamp}_nohup_yolov8l_OWOD_t1_scratch_lr0005.log 2>&1 &
+nohup python custom_training.py -e 300 --lr 0.001 --lrf 0.1 --owod_task t1 --model yolov8 --model_size l --devices 3 --dataset owod --batch_size 16 -cl_ms 5 --workers 12 --val_every 5 --from_scratch > logs/{$timestamp}_nohup_yolov8l_OWOD_t1_scratch_lr0001.log 2>&1 &
+nohup python custom_training.py -e 300 --lr 0.0005 --lrf 0.1 --owod_task t1 --model yolov8 --model_size l --devices 2 --dataset owod --batch_size 16 -cl_ms 5 --workers 12 --val_every 5 --from_scratch > logs/{$timestamp}_nohup_yolov8l_OWOD_t1_scratch_lr00005.log 2>&1 &
+nohup python custom_training.py -e 300 --lr 0.001 --lrf 0.1 --cos_lr --owod_task t1 --model yolov8 --model_size l --devices 1 --dataset owod --batch_size 16 -cl_ms 5 --workers 12 --val_every 5 --from_scratch > logs/{$timestamp}_nohup_yolov8l_OWOD_t1_scratch_lr0001_cosineLR.log 2>&1 &
+nohup python custom_training.py -e 300 --lr 0.0005 --lrf 0.2 --owod_task t1 --model yolov8 --model_size l --devices 2 --dataset owod --batch_size 16 -cl_ms 1 --workers 12 --val_every 5 --from_scratch > logs/{$timestamp}_nohup_yolov8l_OWOD_t1_scratch_lr00005_lf02.log 2>&1 &
+nohup python custom_training.py -e 300 --lr 0.001 --lrf 0.15 --owod_task t1 --model yolov8 --model_size l --devices 3 --dataset owod --batch_size 16 -cl_ms 5 --workers 12 --val_every 5 --from_scratch > logs/{$timestamp}_nohup_yolov8l_OWOD_t1_scratch_lr0001_lf015.log 2>&1 &
+
+nohup python custom_training.py -e 300 --lr 0.001 --lrf 0.15 --owod_task t1 --model yolov5 --model_size l --devices 3 --dataset owod --batch_size 16 -cl_ms 5 --workers 12 --val_every 5 --from_scratch > logs/{$timestamp}_nohup_yolov5l_OWOD_t1_scratch_lr0001_lf015.log 2>&1 &
+
+nohup python custom_training.py --imagenet_pretrained_backbone -e 300 --lr 0.001 --lrf 0.15 --owod_task t1 --model yolov5 --model_size l --devices 3 --dataset owod --batch_size 16 -cl_ms 5 --workers 12 --val_every 5 --from_scratch > logs/{$timestamp}_nohup_yolov8l_OWOD_t1_imagenet_pretrained_lr0001_lf015.log 2>&1 &
+
+
 
 
 
--- scripts/umap_representations.sh
+++ scripts/umap_representations.sh
@@ -1,21 +1,17 @@
-# M models
-nohup python create_umap_representation.py --model_folder runs_TAO/20240229_1946_tao_coco_5_classes_w_imgs_wthout_ann_yolov8m_pretrained --number_of_known_classes 5 --one_umap_per_stride True --device 2 --grid_search_umap True > umap_grid_search_5_cls_M.log &
-
-# X models pretrained
-nohup python create_umap_representation.py --model_folder runs_TAO/20240301_1816_tao_coco_5_classes_yolov8x_pretrained --number_of_known_classes 5 --one_umap_per_stride True --device 2 --grid_search_umap True > umap_grid_search_5_cls.log &
-nohup python create_umap_representation.py --model_folder runs_TAO/20240301_1816_tao_coco_10_classes_yolov8x_pretrained --number_of_known_classes 10 --one_umap_per_stride True --device 1 --grid_search_umap True > umap_grid_search_10_cls.log &
-
-# X models SCRATCH
-python create_umap_representation.py --model_folder runs_TAO/20240305_1150_tao_coco_5_classes_yolov8n_from_scratch --number_of_known_classes 5 --one_umap_per_stride True --device 1 --grid_search_umap False
-
 ### Models from scratch in COCO ###
-python create_umap_representation.py --device 0 --model_folder runs_COCO/20240306_1407_coco_5_classes_yolov8n_from_scratch --dataset tao_coco --split train --number_of_known_classes 5 --one_umap_per_stride True  --grid_search_umap False
-python create_umap_representation.py --device 0 --model_folder runs_COCO/20240306_1407_coco_5_classes_yolov8n_from_scratch --dataset coco --split val --number_of_known_classes 5 --one_umap_per_stride True --grid_search_umap False
-python create_umap_representation.py --device 0 --model_folder runs_COCO/20240306_1406_coco_5_classes_yolov8x_from_scratch --dataset tao_coco --split train --number_of_known_classes 5 --one_umap_per_stride True --grid_search_umap False
-python create_umap_representation.py --device 0 --model_folder runs_COCO/20240306_1406_coco_5_classes_yolov8x_from_scratch --dataset coco --split val --number_of_known_classes 5 --one_umap_per_stride True --grid_search_umap False
+# Grid search
 nohup python create_umap_representation.py --device 2 --model_folder runs_COCO/20240306_1406_coco_5_classes_yolov8x_from_scratch --dataset coco --split val --number_of_known_classes 5 --one_umap_per_stride True --grid_search_umap True > umap_grid_search_5_cls_COCO.log 2>&1 &
-nohup python create_umap_representation.py --device 2 --model_folder runs_COCO/20240306_1406_coco_5_classes_yolov8x_from_scratch --dataset tao_coco --split train --number_of_known_classes 5 --one_umap_per_stride True --grid_search_umap True > umap_grid_search_5_cls_TAO_coco.log 2>&1 &
+nohup python create_umap_representation.py --device 2 --model_folder runs_COCO/20240306_1406_coco_5_classes_yolov8x_from_scratch --dataset tao_coco --split val --number_of_known_classes 5 --one_umap_per_stride True --grid_search_umap True > umap_grid_search_5_cls_TAO_coco.log 2>&1 &
+# Direct
+nohup python create_umap_representation.py --device 0 --model_folder runs_COCO/20240306_1406_coco_5_classes_yolov8x_from_scratch --number_of_known_classes 5 --n_neighbors 20 --metric euclidean --min_dist 0.1 --target_weight 0.2 --one_umap_per_stride True --grid_search_umap False --mode pca_umap --dataset coco --split val &
+nohup python create_umap_representation.py --device 1 --model_folder runs_COCO/20240306_1406_coco_5_classes_yolov8x_from_scratch --number_of_known_classes 5 --n_neighbors 20 --metric euclidean --min_dist 0.1 --target_weight 0.2 --one_umap_per_stride True --grid_search_umap False --mode pca_umap --dataset tao_coco --split val &
+
+nohup python create_umap_representation.py --device 0 --model_folder runs_COCO/20240306_1406_coco_5_classes_yolov8x_from_scratch --number_of_known_classes 5 --n_neighbors 20 --metric euclidean --min_dist 0.1 --target_weight 0.2 --one_umap_per_stride True --grid_search_umap False --mode pca --dataset coco --split val &
+nohup python create_umap_representation.py --device 1 --model_folder runs_COCO/20240306_1406_coco_5_classes_yolov8x_from_scratch --number_of_known_classes 5 --n_neighbors 20 --metric euclidean --min_dist 0.1 --target_weight 0.2 --one_umap_per_stride True --grid_search_umap False --mode pca --dataset tao_coco --split val &
+
+nohup python create_umap_representation.py --device 0 --model_folder runs_COCO/20240306_1406_coco_5_classes_yolov8x_from_scratch --number_of_known_classes 5 --n_neighbors 20 --metric euclidean --min_dist 0.1 --target_weight 0.2 --one_umap_per_stride True --grid_search_umap False --mode umap --dataset coco --split val &
+nohup python create_umap_representation.py --device 1 --model_folder runs_COCO/20240306_1406_coco_5_classes_yolov8x_from_scratch --number_of_known_classes 5 --n_neighbors 20 --metric euclidean --min_dist 0.1 --target_weight 0.2 --one_umap_per_stride True --grid_search_umap False --mode umap --dataset tao_coco --split val &
 
 # Finetuned in TAO
-python create_umap_representation.py --device 0 --model_folder runs_TAO/20240307_1426_tao_coco_5_classes_yolov8_finetuned --dataset tao_coco --split train --number_of_known_classes 5 --one_umap_per_stride True --grid_search_umap False
+python create_umap_representation.py --device 0 --model_folder runs_TAO/20240307_1426_tao_coco_5_classes_yolov8_finetuned --dataset tao_coco --split val --number_of_known_classes 5 --one_umap_per_stride True --grid_search_umap False
 python create_umap_representation.py --device 0 --model_folder runs_TAO/20240307_1426_tao_coco_5_classes_yolov8_finetuned --dataset coco --split val --number_of_known_classes 5 --one_umap_per_stride True --grid_search_umap False
