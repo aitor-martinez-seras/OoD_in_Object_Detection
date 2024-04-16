5560b0bf0db014ef6a5212533cb01b4ab109e8e1
Aitor <aitor.martinez@tecnalia.com>
2024-04-16 16:51:47 +0200

    New folder and new scripts for benchmark

--- scripts/ood_evaluation.sh
+++ scripts/ood_evaluation.sh
@@ -14,3 +14,5 @@ python ood_evaluation.py --load_ind_activations --conf_thr 0.01  --tpr_thr 0.95 
 python ood_evaluation.py --conf_thr 0.0001  --tpr_thr 0.95 --ind_dataset owod --ind_split train --owod_task_ind t1 --ood_dataset owod --ood_split val --owod_task_ood all_task_test --device 3 --ood_method L2_cl_stride --visualize_oods --model_path runs_OWOD/20240313_1407_owod_t1_yolov8l_from_scratch/weights/best.pt
 python ood_evaluation.py --conf_thr 0.001  --tpr_thr 0.95 --ind_dataset owod --ind_split train --owod_task_ind t1 --ood_dataset owod --ood_split val --owod_task_ood all_task_test --device 3 --ood_method L1_cl_stride --visualize_oods --model_path runs_OWOD/20240313_1407_owod_t1_yolov8l_from_scratch/weights/best.pt
 
+
+python ood_evaluation.py --conf_thr 0.15 --tpr_thr 0.95 --ind_dataset owod --ind_split train --owod_task_ind t1 --ood_dataset coco_mixed --ood_split val --owod_task_ood all_task_test --device 3 --ood_method Cosine_cl_stride --compute_metrics --model_path runs_OWOD/20240313_1407_owod_t1_yolov8l_from_scratch/weights/best.pt
