b2323fe67211e12ebca060544fb9877ecc5412a5
Aitor <aitor.martinez@tecnalia.com>
2024-06-05 12:11:39 +0200

    remove old code

--- visualization_utils.py
+++ visualization_utils.py
@@ -10,148 +10,148 @@ from torchvision.utils import draw_bounding_boxes
 from ultralytics.yolo.engine.results import Results
 
 
-def plot_results(
-        class_names: List[str],
-        results: List[Results],
-        folder_path: Path,
-        now: str,
-        valid_preds_only: bool,
-        origin_of_idx: int,
-        image_format: str = 'pdf',
-        ood_decision: Union[List[int], None] = None,
-        ood_method_name: str = '',
-        targets: Union[Dict[str, Tensor], None] = None,
-        possible_unk_boxes_and_decisions: Union[Tuple[List[np.ndarray], List[np.ndarray]], None] = None
-    ):
-    # ----------------------
-    ### Codigo para dibujar las cajas predichas y los targets ###
-    # ----------------------
-    # Parametros para plot
-    width = 2
-    font = 'FreeMonoBold'
-    font_size = 12
+# def plot_results(
+#         class_names: List[str],
+#         results: List[Results],
+#         folder_path: Path,
+#         now: str,
+#         valid_preds_only: bool,
+#         origin_of_idx: int,
+#         image_format: str = 'pdf',
+#         ood_decision: Union[List[int], None] = None,
+#         ood_method_name: str = '',
+#         targets: Union[Dict[str, Tensor], None] = None,
+#         possible_unk_boxes_and_decisions: Union[Tuple[List[np.ndarray], List[np.ndarray]], None] = None
+#     ):
+#     # ----------------------
+#     ### Codigo para dibujar las cajas predichas y los targets ###
+#     # ----------------------
+#     # Parametros para plot
+#     width = 2
+#     font = 'FreeMonoBold'
+#     font_size = 12
 
-    if ood_decision:
-        assert ood_method_name is not None, "If ood_decision exists, ood_method must be a string with the name of the OoD method"
+#     if ood_decision:
+#         assert ood_method_name is not None, "If ood_decision exists, ood_method must be a string with the name of the OoD method"
 
-    # Create folder to store images
-    if ood_method_name:
-        prueba_ahora_path = folder_path / f'{now}_{ood_method_name}'
-    else:
-        prueba_ahora_path = folder_path / now
-    prueba_ahora_path.mkdir(exist_ok=True)
+#     # Create folder to store images
+#     if ood_method_name:
+#         prueba_ahora_path = folder_path / f'{now}_{ood_method_name}'
+#     else:
+#         prueba_ahora_path = folder_path / now
+#     prueba_ahora_path.mkdir(exist_ok=True)
 
-    for img_idx, res in enumerate(results):
+#     for img_idx, res in enumerate(results):
         
-        if valid_preds_only:
-            valid_preds = np.array(res.valid_preds)
-            bboxes = res.boxes.xyxy.cpu()[valid_preds]
-            labels = res.boxes.cls.cpu()[valid_preds]
-        else:
-            bboxes = res.boxes.xyxy.cpu()
-            labels = res.boxes.cls.cpu()
+#         if valid_preds_only:
+#             valid_preds = np.array(res.valid_preds)
+#             bboxes = res.boxes.xyxy.cpu()[valid_preds]
+#             labels = res.boxes.cls.cpu()[valid_preds]
+#         else:
+#             bboxes = res.boxes.xyxy.cpu()
+#             labels = res.boxes.cls.cpu()
 
-            # Este codigo es por si queremos que las cajas OoD se pinten con nombre OoD
-            # for i, decision in enumerate(ood_decision[img_idx]):
-            #     if decision == 0:
-            #         labels[i] = 0
-            # class_names[0] = 'OoD'
+#             # Este codigo es por si queremos que las cajas OoD se pinten con nombre OoD
+#             # for i, decision in enumerate(ood_decision[img_idx]):
+#             #     if decision == 0:
+#             #         labels[i] = 0
+#             # class_names[0] = 'OoD'
 
-            # labels_for_plot = []
-            # for i, lbl in enumerate(labels):
-            #     if lbl == 0:
-            #         labels_for_plot.append(f'{class_names[int(n.item())]} - {res.boxes.conf[i]:.2f}')
-            #     else:    
-            #         labels_for_plot.append(f'{class_names[int(n.item())]} - {res.boxes.conf[i]:.2f}') 
+#             # labels_for_plot = []
+#             # for i, lbl in enumerate(labels):
+#             #     if lbl == 0:
+#             #         labels_for_plot.append(f'{class_names[int(n.item())]} - {res.boxes.conf[i]:.2f}')
+#             #     else:    
+#             #         labels_for_plot.append(f'{class_names[int(n.item())]} - {res.boxes.conf[i]:.2f}') 
         
-        # If we have OoD labels, we plot the boxes with green for In-Distribution and red for OoD
-        if ood_decision:
-            if targets:
-                # Append bboxes of the targets and its labels
-                # Expand the ood_decision to the labels and assign the number n = -5 to the targets
-                # Then paint the targets in violet
+#         # If we have OoD labels, we plot the boxes with green for In-Distribution and red for OoD
+#         if ood_decision:
+#             if targets:
+#                 # Append bboxes of the targets and its labels
+#                 # Expand the ood_decision to the labels and assign the number n = -5 to the targets
+#                 # Then paint the targets in violet
                 
-                bboxes = torch.cat((bboxes, targets["bboxes"][img_idx]), dim=0)
-                labels = torch.cat((labels, targets["cls"][img_idx]), dim=0)
-                ood_decision_one_image = ood_decision[img_idx]
-                ood_decision_one_image = np.append(ood_decision_one_image, [-5]*len(targets["cls"][img_idx]))
-                colors = []
-                for decision in ood_decision_one_image:
-                    if decision == 1:
-                        colors.append('green')
-                    elif decision == -5:
-                        colors.append('violet')
-                    elif decision == 0:
-                        colors.append('red')
-                    else:
-                        raise ValueError("The OoD decision must be 0, 1 or -5")
+#                 bboxes = torch.cat((bboxes, targets["bboxes"][img_idx]), dim=0)
+#                 labels = torch.cat((labels, targets["cls"][img_idx]), dim=0)
+#                 ood_decision_one_image = ood_decision[img_idx]
+#                 ood_decision_one_image = np.append(ood_decision_one_image, [-5]*len(targets["cls"][img_idx]))
+#                 colors = []
+#                 for decision in ood_decision_one_image:
+#                     if decision == 1:
+#                         colors.append('green')
+#                     elif decision == -5:
+#                         colors.append('violet')
+#                     elif decision == 0:
+#                         colors.append('red')
+#                     else:
+#                         raise ValueError("The OoD decision must be 0, 1 or -5")
                     
-                labels_str = []
-                for i, lbl in enumerate(labels):
-                    if ood_decision_one_image[i] == -5:
-                        labels_str.append(f'{class_names[int(lbl.item())]} - GT')
-                    else:
-                        labels_str.append(f'{class_names[int(lbl.item())]} - {res.boxes.conf[i]:.2f}')
+#                 labels_str = []
+#                 for i, lbl in enumerate(labels):
+#                     if ood_decision_one_image[i] == -5:
+#                         labels_str.append(f'{class_names[int(lbl.item())]} - GT')
+#                     else:
+#                         labels_str.append(f'{class_names[int(lbl.item())]} - {res.boxes.conf[i]:.2f}')
 
-                im = draw_bounding_boxes(
-                    res.orig_img[img_idx].cpu(),
-                    bboxes,
-                    width=width,
-                    font=font,
-                    font_size=font_size,
-                    labels=labels_str,
-                    colors=colors
-                )
-            else:
-                im = draw_bounding_boxes(
-                    res.orig_img[img_idx].cpu(),
-                    bboxes,
-                    width=width,
-                    font=font,
-                    font_size=font_size,
-                    labels=[f'{class_names[int(n.item())]} - {res.boxes.conf[i]:.2f}' for i, n in enumerate(labels)],
-                    colors=['red' if n == 0 else 'green' for n in ood_decision[img_idx]]
-                )
+#                 im = draw_bounding_boxes(
+#                     res.orig_img[img_idx].cpu(),
+#                     bboxes,
+#                     width=width,
+#                     font=font,
+#                     font_size=font_size,
+#                     labels=labels_str,
+#                     colors=colors
+#                 )
+#             else:
+#                 im = draw_bounding_boxes(
+#                     res.orig_img[img_idx].cpu(),
+#                     bboxes,
+#                     width=width,
+#                     font=font,
+#                     font_size=font_size,
+#                     labels=[f'{class_names[int(n.item())]} - {res.boxes.conf[i]:.2f}' for i, n in enumerate(labels)],
+#                     colors=['red' if n == 0 else 'green' for n in ood_decision[img_idx]]
+#                 )
         
-        # Just plot normal predictions
-        else:
-            if targets:
-                bboxes_preds_plus_gt = torch.cat((bboxes, targets["bboxes"][img_idx]), dim=0)
-                #labels_preds_plus_gt = torch.cat((labels, targets["cls"][img_idx]), dim=0)
-                # import random
-                # get_colors = lambda n: ["#%06x" % random.randint(0, 0xFFFFFF) for _ in range(n)]
-                # colors = get_colors(len(labels))
-                #colors.extend(['violet']*len(targets["cls"][img_idx]))
-                colors = ['green']*len(labels) + ['violet']*len(targets["cls"][img_idx])
+#         # Just plot normal predictions
+#         else:
+#             if targets:
+#                 bboxes_preds_plus_gt = torch.cat((bboxes, targets["bboxes"][img_idx]), dim=0)
+#                 #labels_preds_plus_gt = torch.cat((labels, targets["cls"][img_idx]), dim=0)
+#                 # import random
+#                 # get_colors = lambda n: ["#%06x" % random.randint(0, 0xFFFFFF) for _ in range(n)]
+#                 # colors = get_colors(len(labels))
+#                 #colors.extend(['violet']*len(targets["cls"][img_idx]))
+#                 colors = ['green']*len(labels) + ['violet']*len(targets["cls"][img_idx])
                 
-                labels_str = []
-                for i, lbl in enumerate(labels):
-                    # Check if they are predictions  for this image
-                    labels_str.append(f'{class_names[int(lbl.item())]} - {res.boxes.conf[i]:.2f}')
-                for i, lbl in enumerate(targets["cls"][img_idx]):
-                    labels_str.append(f'{class_names[int(lbl.item())]} - GT')
-                im = draw_bounding_boxes(
-                    res.orig_img[img_idx].cpu(),
-                    bboxes_preds_plus_gt,
-                    width=width,
-                    font=font,
-                    font_size=font_size,
-                    labels=labels_str,
-                    colors=colors
-                )
-            else:
-                im = draw_bounding_boxes(
-                    res.orig_img[img_idx].cpu(),
-                    bboxes,
-                    width=5,
-                    font=font,
-                    font_size=font_size,
-                    labels=[f'{class_names[int(n.item())]} - {res.boxes.conf[i]:.2f}' for i, n in enumerate(labels)]
-                )
+#                 labels_str = []
+#                 for i, lbl in enumerate(labels):
+#                     # Check if they are predictions  for this image
+#                     labels_str.append(f'{class_names[int(lbl.item())]} - {res.boxes.conf[i]:.2f}')
+#                 for i, lbl in enumerate(targets["cls"][img_idx]):
+#                     labels_str.append(f'{class_names[int(lbl.item())]} - GT')
+#                 im = draw_bounding_boxes(
+#                     res.orig_img[img_idx].cpu(),
+#                     bboxes_preds_plus_gt,
+#                     width=width,
+#                     font=font,
+#                     font_size=font_size,
+#                     labels=labels_str,
+#                     colors=colors
+#                 )
+#             else:
+#                 im = draw_bounding_boxes(
+#                     res.orig_img[img_idx].cpu(),
+#                     bboxes,
+#                     width=5,
+#                     font=font,
+#                     font_size=font_size,
+#                     labels=[f'{class_names[int(n.item())]} - {res.boxes.conf[i]:.2f}' for i, n in enumerate(labels)]
+#                 )
 
-        plt.imshow(im.permute(1,2,0))
-        plt.savefig(prueba_ahora_path / f'{(origin_of_idx + img_idx):03}.{image_format}', dpi=300)
-        plt.close()
+#         plt.imshow(im.permute(1,2,0))
+#         plt.savefig(prueba_ahora_path / f'{(origin_of_idx + img_idx):03}.{image_format}', dpi=300)
+#         plt.close()
 
 
 
