c778d8bb8ddebc82647cb83a1bfb8c16bcbf80d2
Aitor <aitor.martinez@tecnalia.com>
2025-05-18 16:47:36 +0200

    Add new training commands

--- replicate/train.sh
+++ replicate/train.sh
@@ -1,7 +1,9 @@
 #nohup python custom_training.py --model custom_yolov8 -e 300 --lr 0.0005 --lrf 0.2 --owod_task t1 --model_size l --devices 2 --dataset owod --batch_size 16 -cl_ms 1 --workers 12 --val_every 5 --from_scratch > logs/nohup_custom_yolov8l_OWOD_t1_scratch_lr00005_lf02.log 2>&1 &
 # V11
+nohup python custom_training.py --model yolo11 -e 300 --lr 0.0003 --lrf 0.2 --owod_task t1 --model_size l --devices 0 --dataset owod --batch_size 16 -cl_ms 1 --workers 12 --val_every 5 --from_scratch > logs/nohup_custom_yolo11l_OWOD_t1_scratch_lr00003_lf02.log 2>&1 &
 nohup python custom_training.py --model yolo11 -e 300 --lr 0.0005 --lrf 0.2 --owod_task t1 --model_size l --devices 0 --dataset owod --batch_size 16 -cl_ms 1 --workers 12 --val_every 5 --from_scratch > logs/nohup_custom_yolo11l_OWOD_t1_scratch_lr00005_lf02.log 2>&1 &
 nohup python custom_training.py --model yolo11 -e 300 --lr 0.001 --lrf 0.2 --owod_task t1 --model_size l --devices 0 --dataset owod --batch_size 16 -cl_ms 1 --workers 12 --val_every 5 --from_scratch > logs/nohup_custom_yolo11l_OWOD_t1_scratch_lr0001_lf02.log 2>&1 &
 
 # V12
-nohup python custom_training.py --model yolo12 -e 300 --lr 0.0005 --lrf 0.2 --owod_task t1 --model_size l --devices 0 --dataset owod --batch_size 16 -cl_ms 1 --workers 12 --val_every 5 --from_scratch > logs/nohup_custom_yolo12l_OWOD_t1_scratch_lr0001_lf02.log 2>&1 &
+nohup python custom_training.py --model yolo12 -e 300 --lr 0.0005 --lrf 0.2 --owod_task t1 --model_size l --devices 0 --dataset owod --batch_size 16 -cl_ms 1 --workers 12 --val_every 5 --from_scratch > logs/nohup_custom_yolo12l_OWOD_t1_scratch_lr00005_lf02.log 2>&1 &
+nohup python custom_training.py --model yolo12 -e 300 --lr 0.001 --lrf 0.2 --owod_task t1 --model_size l --devices 0 --dataset owod --batch_size 16 -cl_ms 1 --workers 12 --val_every 5 --from_scratch > logs/nohup_custom_yolo12l_OWOD_t1_scratch_lr0001_lf02.log 2>&1 &
