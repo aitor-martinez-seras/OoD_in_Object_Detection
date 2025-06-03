9b6c23bd438f40aba26d4f1b02fcee2b09281eca
Aitor <aitor.martinez@tecnalia.com>
2025-06-03 23:51:54 +0200

    Use old compose function for transforms

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -2535,17 +2535,25 @@ def v8_transforms(dataset, imgsz, hyp, stretch=False):
         elif flip_idx and (len(flip_idx) != kpt_shape[0]):
             raise ValueError(f"data.yaml flip_idx={flip_idx} length must be equal to kpt_shape[0]={kpt_shape[0]}")
 
-    return Compose(
-        [
-            pre_transform,
-            MixUp(dataset, pre_transform=pre_transform, p=hyp.mixup),
-            CutMix(dataset, pre_transform=pre_transform, p=hyp.cutmix),
-            Albumentations(p=1.0),
-            RandomHSV(hgain=hyp.hsv_h, sgain=hyp.hsv_s, vgain=hyp.hsv_v),
-            RandomFlip(direction="vertical", p=hyp.flipud),
-            RandomFlip(direction="horizontal", p=hyp.fliplr, flip_idx=flip_idx),
-        ]
-    )  # transforms
+    # return Compose(
+    #     [
+    #         pre_transform,
+    #         MixUp(dataset, pre_transform=pre_transform, p=hyp.mixup),
+    #         CutMix(dataset, pre_transform=pre_transform, p=hyp.cutmix),
+    #         Albumentations(p=1.0),
+    #         RandomHSV(hgain=hyp.hsv_h, sgain=hyp.hsv_s, vgain=hyp.hsv_v),
+    #         RandomFlip(direction="vertical", p=hyp.flipud),
+    #         RandomFlip(direction="horizontal", p=hyp.fliplr, flip_idx=flip_idx),
+    #     ]
+    # )  # transforms
+
+    return Compose([
+    pre_transform,
+    MixUp(dataset, pre_transform=pre_transform, p=hyp.mixup),
+    Albumentations(p=1.0),
+    RandomHSV(hgain=hyp.hsv_h, sgain=hyp.hsv_s, vgain=hyp.hsv_v),
+    RandomFlip(direction='vertical', p=hyp.flipud),
+    RandomFlip(direction='horizontal', p=hyp.fliplr, flip_idx=flip_idx)])  # transforms
 
 
 # Classification augmentations -----------------------------------------------------------------------------------------
